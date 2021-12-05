fc1325282dafb6f7b02c227585cf90127f35d407
WAO-Z <576816951@QQ.COM>
2021-12-05 22:09:03 +0800

    抽离商品组件

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -6,23 +6,9 @@
 			</u-tabs>
 		</view>
 		<u-row class="u-skeleton" gutter="16">
-			<u-col span="6" v-for="item in goods.length !==0 ? goods : 6">
+			<u-col span="6" v-for="goods in goodsList.length !==0 ? goodsList : [{},{},{},{}]">
 
-				<navigator class="goods-item">
-					<u-image class="u-skeleton-fillet" width='100%' height="300rpx" :src="item.cover_url">
-					</u-image>
-					<view class="title u-line-1 u-skeleton-fillet">
-						{{item.title ? item.title : "商品名称" }}
-					</view>
-					<view class="u-flex u-row-between">
-						<view class="price u-skeleton-fillet">
-							¥ {{item.price}}
-						</view>
-						<view class="sales u-skeleton-fillet">
-							销量：{{item.sales}}
-						</view>
-					</view>
-				</navigator>
+				<goods-card :goods="goods"></goods-card>
 
 			</u-col>
 
@@ -52,7 +38,7 @@
 				],
 				currentSort: 0,
 				slides: [],
-				goods: [],
+				goodsList: [],
 				loading: false
 			}
 		},
@@ -66,7 +52,7 @@
 				console.log(index)
 				this.currentSort = index
 				//重置商品数据和分页
-				this.goods = []
+				this.goodsList = []
 				this.page = 1
 				this.getData()
 			},
@@ -83,7 +69,7 @@
 				const res = await this.$u.api.index(params)
 				this.loading = false
 				this.slides = res.slides
-				this.goods = [...this.goods, ...res.goods.data]
+				this.goodsList = [...this.goodsList, ...res.goods.data]
 			}
 		},
 		onReachBottom() {
@@ -95,25 +81,4 @@
 </script>
 
 <style lang="scss" scoped>
-	.goods-item {
-		padding: 40rpx;
-		margin-top: 30rpx;
-		box-shadow: 0 12rpx 20rpx 0 rgba(0, 0, 0, .1);
-	}
-
-	.title {
-		margin: 10rpx 0;
-		font-weight: 500;
-		font-size: 32rpx;
-	}
-
-	.price {
-		color: red;
-		width: 40%;
-	}
-
-	.sales {
-		color: #888;
-		width: 40%;
-	}
 </style>
