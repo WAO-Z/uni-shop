aea9ad4fcf8df9f4f00d901cb0506af1d8f2f036
WAO-Z <576816951@QQ.COM>
2021-12-05 21:41:55 +0800

    首页

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,26 +1,25 @@
 <template>
 	<view>
-		<u-swiper :list='imgList' img-mode="widthFix" height=""></u-swiper>
+		<u-swiper :list='slides' img-mode="widthFix" height="320" name="img_url"></u-swiper>
 		<view class="u-text-center u-m-t-30">
 			<u-tabs :list="sortList" bar-width="100" item-width="160" :current="currentSort" @change="changeSort">
 			</u-tabs>
 		</view>
-		<u-row gutter="16">
-			<u-col span="6" v-for="i in 12">
+		<u-row class="u-skeleton" gutter="16">
+			<u-col span="6" v-for="item in goods.length !==0 ? goods : 6">
 
 				<navigator class="goods-item">
-					<u-image width='100%' height="300rpx"
-						src="https://oss.shop.eduwork.cn//ad/2020-0820-5f3dfe7756215.png">
+					<u-image class="u-skeleton-fillet" width='100%' height="300rpx" :src="item.cover_url">
 					</u-image>
-					<view class="title">
-						区块链
+					<view class="title u-line-1 u-skeleton-fillet">
+						{{item.title ? item.title : "商品名称" }}
 					</view>
 					<view class="u-flex u-row-between">
-						<view class="price">
-							¥ 99
+						<view class="price u-skeleton-fillet">
+							¥ {{item.price}}
 						</view>
-						<view class="sales">
-							销量：10
+						<view class="sales u-skeleton-fillet">
+							销量：{{item.sales}}
 						</view>
 					</view>
 				</navigator>
@@ -28,17 +27,16 @@
 			</u-col>
 
 		</u-row>
+		<!--引用组件-->
+		<u-skeleton :loading="loading" :animation="true" bgColor="#FFF"></u-skeleton>
 	</view>
+
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				imgList: [
-					'https://images.weserv.nl/?url=https://i0.hdslb.com/bfs/feed-admin/27be9cbc8de68388612913cf84f976c1dbbf2c54.jpg@336w_190h_1c.jpg',
-					'https://images.weserv.nl/?url=https://i1.hdslb.com/bfs/archive/b6e0daf6fb725ad2e010ce27c11d818ed0c10e79.png@336w_190h_1c.png'
-				],
 				sortList: [{
 						name: '默认'
 					},
@@ -52,25 +50,46 @@
 						name: '最新'
 					}
 				],
-				currentSort: 0
+				currentSort: 0,
+				slides: [],
+				goods: [],
+				loading: false
 			}
 		},
 
-		async onLoad() {
-			const data = {
-				email: 'test@a.com',
-				password: '123123'
-			}
-			// const res = this.$u.api.authLogin(data)
-			// console.log(res)
-			// console.log(this.vuex_version)
-			// this.$u.vuex('vuex_name', 'Tom')
-			// console.log(this.vuex_name)
+		onLoad() {
+			this.getData()
+
 		},
 		methods: {
 			changeSort(index) {
+				console.log(index)
 				this.currentSort = index
+				//重置商品数据和分页
+				this.goods = []
+				this.page = 1
+				this.getData()
+			},
+			//获取数据
+			async getData() {
+				this.loading = true
+				const params = {
+					page: this.page
+				}
+				//增加排序条件
+				if (this.currentSort == 1) params.sales = 1
+				if (this.currentSort == 2) params.recommend = 1
+				if (this.currentSort == 3) params.new = 1
+				const res = await this.$u.api.index(params)
+				this.loading = false
+				this.slides = res.slides
+				this.goods = [...this.goods, ...res.goods.data]
 			}
+		},
+		onReachBottom() {
+			//重新请求数据，带上分页参数
+			this.page = this.page + 1
+			this.getData()
 		}
 	}
 </script>
@@ -90,9 +109,11 @@
 
 	.price {
 		color: red;
+		width: 40%;
 	}
 
 	.sales {
 		color: #888;
+		width: 40%;
 	}
 </style>
